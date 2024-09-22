3201117bd82adb8ca99d7551d60dc652f1d2b8b4
aloui_houssem <aloui9861@gmail.com>
2024-09-22 22:03:36 +0200

    initiate the project

---
+++ src/main/java/com/enicarthage/sreviceDeStage/SreviceDeStageApplication.java
@@ -0,0 +1,13 @@
+package com.enicarthage.sreviceDeStage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreviceDeStageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreviceDeStageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enicarthage/sreviceDeStage/SreviceDeStageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enicarthage.sreviceDeStage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SreviceDeStageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
